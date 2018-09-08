a5c2a1527ac07a5d4f8c6b80b43bea21c580994a
Wang Gang <w.gang6008@gmail.com>
2018-09-09 01:23:36 +1000

    First commit

---
+++ AlamofireObjectMapperKit/Services/GenericService.swift
@@ -0,0 +1,13 @@
+//
+//  GenericService.swift
+//  AlamofireObjectMapperKit
+//
+//  Created by David Bala on 09/09/2018.
+//  Copyright © 2018 AXNET. All rights reserved.
+//
+
+import UIKit
+
+class GenericService: NSObject {
+
+}
