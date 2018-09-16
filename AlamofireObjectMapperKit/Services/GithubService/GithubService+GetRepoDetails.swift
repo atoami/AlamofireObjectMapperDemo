dc0b1bc19c01e8cd451d4f5b42780c3ca7908a57
Wang Gang <w.gang6008@gmail.com>
2018-09-17 02:44:54 +1000

    Get repo detail api

---
+++ AlamofireObjectMapperKit/Services/GithubService/GithubService+GetRepoDetails.swift
@@ -0,0 +1,13 @@
+//
+//  GithubService+GetRepoDetails.swift
+//  AlamofireObjectMapperKit
+//
+//  Created by BBDroid on 17/09/2018.
+//  Copyright © 2018 AXNET. All rights reserved.
+//
+
+import UIKit
+
+extension GithubService {
+    
+}
